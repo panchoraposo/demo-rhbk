f2ad76ca3daf28bad3db1eb74957b8e7b612f9b5
Francisco Raposo <fraposo@redhat.com>
2025-01-27 19:08:09 -0300

    frontend keycloak

--- frontend/src/keycloak.js
+++ frontend/src/keycloak.js
@@ -2,7 +2,7 @@
 import Keycloak from 'keycloak-js';
 
 const keycloak = new Keycloak({
-    url:  process.env.KEYCLOAK_URL, // Keycloak base URL
+    url:  process.env.KEYCLOAK_URL || "https://keycloak.apps.i7mvwf9w.eastus.aroapp.io", // Keycloak base URL
     realm: 'demo',         // Realm name
     clientId: 'todo-frontend',   // Client ID
 });
