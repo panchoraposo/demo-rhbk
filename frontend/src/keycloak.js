99be7d90961d416b82e8d5f71cef2adf37bea76a
panchoraposo <pancho.raposo@gmail.com>
2025-12-16 13:58:29 -0300

    Update keycloak.js

--- frontend/src/keycloak.js
+++ frontend/src/keycloak.js
@@ -2,7 +2,7 @@
 import Keycloak from 'keycloak-js';
 
 const keycloak = new Keycloak({
-    url:  process.env.KEYCLOAK_URL, // Keycloak base URL
+    url:  process.env.REACT_APP_KEYCLOAK_URL, // Keycloak base URL
     realm: 'demo',         // Realm name
     clientId: 'todo-frontend',   // Client ID
 });
